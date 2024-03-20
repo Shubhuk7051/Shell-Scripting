cf71a061673895823ef72bdfd3a4dbb1b9b37f99
Cloud User <ec2-user@ip-172-31-36-139.ec2.internal>
2024-03-20 07:39:58 +0000

    Add Loops Scripts

---
+++ loops/days.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+i=1
+for day in Mon Tue Wed Thur Fri
+do
+	echo "Weekday $((i++)) : $day"
+done
---
+++ loops/simple_for2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in jenkins terraform AWS DevOps
+do
+	echo "Shubham expert in $i"
+done
+
